bfbc134a3d023b0782fed225cd469eef3564842f
vishalk95 <kapadivishal17@gmail.com>
2018-12-09 05:07:51 -0800

    Create set_custom_variables.sh
    my intention is to put this lines at the end of .bashrc

---
+++ set_custom_variables.sh
@@ -0,0 +1,12 @@
+cd ~/
+echo 'export KBUILD_BUILD_USER=vishalk17' >> ~/.bashrc
+echo 'export KBUILD_BUILD_HOST=vishal-vm' >> ~/.bashrc
+echo "set out User and Host"
+echo "set ccache"
+echo 'export USE_CCACHE=1' >> ~/.bashrc
+ccache -M 60G
+echo "set out ccache"
+echo "Configure Jack to allocate a sufficient amount of memory"
+echo "export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx6G""
+echo "Configured Jack"
+echo "Setout Custom Variables Successfully"
